ef9925a1138a014fc3702ea341ef2c9cae8f65ad
SYED AHMED (RIT Student) <syed.ahmed.emails@gmail.com>
2015-08-12 16:44:50 -0400

    Initial Commit. Working Code.

---
+++ QCARJava/app/src/main/java/com/thesyedahmed/aresume/MainActivity.java
@@ -0,0 +1,19 @@
+package com.thesyedahmed.aresume;
+
+import android.R;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.unity3d.player.UnityPlayerNativeActivity;
+
+
+public class MainActivity extends UnityPlayerNativeActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+    }
+   
+}
