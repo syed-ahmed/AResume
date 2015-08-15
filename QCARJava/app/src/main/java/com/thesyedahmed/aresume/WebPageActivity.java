3de8ab67176e51e3e0b4569a8035a2f3086c381f
Syed Ahmed <syed.ahmed.emails@gmail.com>
2015-08-14 22:56:54 -0400

    Added web history back functionality

--- QCARJava/app/src/main/java/com/thesyedahmed/aresume/WebPageActivity.java
+++ QCARJava/app/src/main/java/com/thesyedahmed/aresume/WebPageActivity.java
@@ -7,12 +7,15 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebBackForwardList;
+import android.webkit.WebView;
 
 /**
  * Created by steakpizza on 8/14/2015.
  */
 public class WebPageActivity extends SingleFragmentActivity {
-        public static Intent newIntent(Context context, Uri webPageUri){
+
+    public static Intent newIntent(Context context, Uri webPageUri){
         Intent i = new Intent(context, WebPageActivity.class);
         i.setData(webPageUri);
         return i;
@@ -22,4 +25,14 @@ public class WebPageActivity extends SingleFragmentActivity {
     protected Fragment createFragment(){
         return WebPageFragment.newInstance(getIntent().getData());
     }
+
+    @Override
+    public void onBackPressed(){
+        if(WebPageFragment.getWebView().canGoBack()){
+            WebPageFragment.getWebView().goBack();
+        }else{
+            super.onBackPressed();
+        }
+    }
+
 }
--- QCARJava/app/src/main/java/com/thesyedahmed/aresume/WebPageFragment.java
+++ QCARJava/app/src/main/java/com/thesyedahmed/aresume/WebPageFragment.java
@@ -4,11 +4,15 @@ package com.thesyedahmed.aresume;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebBackForwardList;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 /**
  * Created by steakpizza on 8/13/2015.
@@ -16,7 +20,8 @@ import android.webkit.WebViewClient;
 public class WebPageFragment extends Fragment {
     private static final String ARG_URI = "web_page_url";
     private Uri mUri;
-    private WebView mWebView;
+    private static WebView mWebView;
+    private ProgressBar mProgressBar;
 
     public static WebPageFragment newInstance (Uri uri){
         Bundle args = new Bundle();
@@ -37,14 +42,36 @@ public class WebPageFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         View v = inflater.inflate(R.layout.fragment_web_page, container, false);
+        mProgressBar = (ProgressBar) v.findViewById(R.id.fragment_web_page_progress_bar);
+        mProgressBar.setMax(100);
         mWebView = (WebView) v.findViewById(R.id.fragment_web_page_web_view);
         mWebView.getSettings().setJavaScriptEnabled(true);
-        mWebView.setWebViewClient(new WebViewClient(){
-            public boolean shouldOverrideUrlLoading(WebView view, String url){
+        mWebView.setWebChromeClient(new WebChromeClient() {
+            public void onProgressChanged(WebView webView, int newProgress) {
+                if (newProgress == 100) {
+                    mProgressBar.setVisibility(View.GONE);
+                } else {
+                    mProgressBar.setVisibility(View.VISIBLE);
+                    mProgressBar.setProgress(newProgress);
+                }
+
+            }
+
+            public void onReceivedTitle(WebView webView, String title) {
+                AppCompatActivity activity = (AppCompatActivity) getActivity();
+                activity.getSupportActionBar().setSubtitle(title);
+            }
+        });
+        mWebView.setWebViewClient(new WebViewClient() {
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
                 return false;
             }
         });
         mWebView.loadUrl(mUri.toString());
         return v;
+    }
+
+    public static WebView getWebView() {
+        return mWebView;
     }
 }
